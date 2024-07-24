cc298f293e0bbca852a7481441f229f4aae134a9
Rutuja Ruke <rutuja.ruke2004@gmail.com>
2024-07-24 11:06:41 +0530

    Footer created

--- KAITSolution/src/Components/Footer/Footer.jsx
+++ KAITSolution/src/Components/Footer/Footer.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import './Footer.css'
+import React from 'react';
+import './Footer.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section company-info">
+          <h3>Company Info</h3>
+          <p>Cake Shop Inc.</p>
+          <p><FontAwesomeIcon icon={faMapMarkerAlt} /> IVF Off, Acolate Society Road, Hadpasar Bypass Road, Kharadi</p>
+          <p><FontAwesomeIcon icon={faPhone} /> (123) 456-7890</p>
+          <p><FontAwesomeIcon icon={faEnvelope} /> kaitsolutions1@gmail.com</p>
+        </div>
+        <div className="footer-section quick-links">
+          <h3>Quick Links</h3>
+          <ul>
+            <li><a href="#about">About Us</a></li>
+            <li><a href="#products">Products</a></li>
+            <li><a href="#contact">Contact Us</a></li>
+            <li><a href="#faq">FAQ</a></li>
+          </ul>
+        </div>
+        <div className="footer-section social-media">
+          <h3>Follow Us</h3>
+          <div className="social-icons">
+            <a href="https://facebook.com"><FontAwesomeIcon icon={faFacebook} /></a>
+            <a href="https://twitter.com"><FontAwesomeIcon icon={faTwitter} /></a>
+            <a href="https://instagram.com"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="https://linkedin.com"><FontAwesomeIcon icon={faLinkedin} /></a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        &copy; {new Date().getFullYear()} KA IT Software Solutions Inc. All Rights Reserved.
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
