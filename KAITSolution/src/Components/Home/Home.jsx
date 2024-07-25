c8aa268744bfa6f372f9c76d69546cb0dc80af77
Rutuja Ruke <rutuja.ruke2004@gmail.com>
2024-07-25 23:07:30 +0530

    design home page using tailwind

--- KAITSolution/src/Components/Home/Home.jsx
+++ KAITSolution/src/Components/Home/Home.jsx
@@ -24,31 +24,76 @@ const Home = () => {
             </div>
             <h2 className="text-2xl font-bold">LET'S BUILD AN EXCEPTIONAL WEBSITE TOGATHER.</h2>
             <p className='discription'>We are the Make Lots Foundation. We design professional websites for those in need. We’re offering our design services at an affordable rate to support small businesses in building their online presence.</p>
-            <div className="text-white p-4 rounded-lg grid-heading">
-              Dark blue div with text
-            </div>
+
+
+
+
+
+
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-              <div className="bg-yellow-300 p-4 rounded-lg">
-                <h3 className="text-xl font-bold">Website Building</h3>
-                <p>Building dynamic and responsive websites</p>
-                <ul className="list-disc ml-4">
-                  <li>Customized Designs</li>
-                  <li>SEO Friendly</li>
-                  <li>Fast Loading</li>
-                </ul>
+
+              <div className="col-span-1 md:col-span-2 bg-dark-blue text-white p-4 rounded-lg grid-heading">
+                <h2 className="text-2xl font-bold text-center">PLEASE SELECT YOUR PACAKGE</h2>
+                <p className="text-center">Once your order is placed, we'll reach out to you shortly.</p>
               </div>
-              <div className="bg-gray-200 p-4 rounded-lg">
-                <h3 className="text-xl font-bold">Price</h3>
-                <p>Affordable pricing for all services</p>
-                <ul className="list-disc ml-4">
-                  <li>Competitive Rates</li>
-                  <li>Transparent Billing</li>
-                  <li>No Hidden Costs</li>
-                </ul>
+
+              <div className="bg-white p-4 rounded-lg shadow-lg mt-0">
+                <div className="grid grid-cols-1 gap-4">
+                  {/* <div className="bg-blue-200 p-4 rounded-t-lg">
+                    <h3 className="text-xl font-bold">Business Pro Website</h3>
+                  </div> */}
+                  <div className=" p-4 rounded-t-lg italic card1-heading">
+                    <h3 className="text-xl font-bold text-center">BUSINESS PRO WEBSITE</h3>
+                  </div>
+                </div>
+                <div className="bg-gray-300 p-4">
+                  <h3 className="text-xl font-bold text-center">$ 200</h3>
+                </div>
+                <div className="bg-white p-4 text-center">
+                  <ul className="list-none">
+                    <li className="py-2 border-b border-gray-300">5-Page Website</li>
+                    <li className="py-2 border-b border-gray-300">Domain Registrstion</li>
+                    <li className="py-2 border-b border-gray-300">Web Hosting*</li>
+                    <li className="py-2 border-b border-gray-300">Business Email</li>
+                    <li className="py-2">SEO Optimized Design</li>
+                  </ul>
+                  <button className="card1-heading text-white p-4 rounded-lg mt-4">Get Started</button>
+                </div>
               </div>
+
+              <div className="bg-white p-4 rounded-lg shadow-lg mt-0">
+                <div className="grid grid-cols-1 gap-4">
+                  {/* <div className="bg-blue-200 p-4 rounded-t-lg">
+                    <h3 className="text-xl font-bold">Business Pro Website</h3>
+                  </div> */}
+                  <div className=" p-4 rounded-t-lg italic card2-heading">
+                    <h3 className="text-xl font-bold text-center">E-COMMERCE WEBSITE</h3>
+                  </div>
+                </div>
+                <div className="bg-gray-300 p-4">
+                  <h3 className="text-xl font-bold text-center">$ 300</h3>
+                </div>
+                <div className="bg-white p-4 text-center">
+                  <ul className="list-none">
+                    <li className="py-2 border-b border-gray-300">10 Products Listed</li>
+                    <li className="py-2 border-b border-gray-300">Domain Registrstion</li>
+                    <li className="py-2 border-b border-gray-300">Web Hosting*</li>
+                    <li className="py-2 border-b border-gray-300">Business Email</li>
+                    <li className="py-2">SEO Optimized Design</li>
+                  </ul>
+                  <button className="card2-heading text-white p-4 rounded-lg mt-4">Get Started</button>
+                </div>
+              </div>
+
             </div>
-            <button className="bg-blue-500 text-white p-4 rounded-lg">Button</button>
+
+
+
+
           </div>
+
+
+
 
           {/* Right side */}
           <div className="w-full md:w-1/2 relative">
@@ -60,15 +105,56 @@ const Home = () => {
                 style={{ clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)' }} // Clipping the image
               />
             </div>
-            <h2 className="text-2xl font-bold mt-4">Some Heading</h2>
-            <ul className="list-disc ml-4 mt-2">
-              <li>Key feature one explained in detail.</li>
-              <li>Another essential point described.</li>
-              <li>Additional information or feature point.</li>
-            </ul>
+            <div className="text-center p-4">
+  <h2 className="text-2xl font-bold mt-4">CRAFTING OUTSTANDING WEB EXPERIENCES</h2>
+  <p className="mt-2">RESPONSIVE, USER-FRIENDLY, VISUALLY STUNNING DESIGNS</p>
+  <ul className="list-none p-2 ml-4 mt-4 text-left inline-block">
+    <li className="flex items-center ">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Responsive Layouts
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Intuitive User Experience
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Visually Striking Designs
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Seamless Navigation
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Accessible Content
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Fast Loading Times
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Engaging Interactions
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Cross-Browser Compatibility
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Engaging Interactions
+    </li>
+    <li className="flex items-center mt-2">
+      <svg className="w-4 h-4 bg-[rgb(39,158,182)] text-white mr-2" fill="currentColor" viewBox="0 0 20 20"><path fillRule="evenodd" d="M16.707 5.707a1 1 0 010 1.414l-7 7a1 1 0 01-1.414 0l-3-3a1 1 0 011.414-1.414L9 11.586l6.293-6.293a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+      Cross-Browser Compatibility
+    </li>
+  </ul>
+</div>
+
           </div>
         </div>
-      </div>
+      </div >
     </>
   );
 }
