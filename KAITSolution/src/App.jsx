8838b798a85bfdc301f0f57383e81b8cbafce485
Rutuja Ruke <rutuja.ruke2004@gmail.com>
2024-07-30 16:15:21 +0530

    dne

--- KAITSolution/src/App.jsx
+++ KAITSolution/src/App.jsx
@@ -26,11 +26,6 @@ function App() {
     </Router>
 
 
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS with Vite!</h1>
-    </div>
-
-
     </>
   )
 }
